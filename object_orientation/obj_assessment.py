532a7f53af0b9079bf406a3e98ec80e688318af8
Michelle <jhmichellekim@gmail.com>
2016-10-09 17:14:52 -0700

    dict_practice

---
+++ object_orientation/obj_assessment.py
@@ -0,0 +1,39 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+Having classes makes possible:
+subclasses that inherit attributes and methods from their parent. This allows us
+to make variations of the parent rather easily.
+Keeps the code from being overly long, confusing, or repetitive.
+-abstraction:
+ don't need to let the user know all the intricacies of how certain functions work
+-encapsulation:
+ data is bundled with the functions that work on that data
+-polymorphism:
+ functions or methods can be applied to more than one subclass — general enough
+
+
+2. What is a class?
+It's a "type" of object that a coder can create, and it usually has a template
+of attributes and that its subclasses can take on
+
+3. What is an instance attribute?
+
+
+4. What is a method?
+
+5. What is an instance in object orientation?
+It's when 
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
